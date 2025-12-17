4422f181e7d2adcdd46e690226e2215881643b7b
thisdcw <2839230683@qq.com>
2025-12-17 16:59:56 +0800

    add: Hot15

---
+++ cw-algorithm/src/main/java/com/cw/algorithm/hot100/Hot15.java
@@ -0,0 +1,55 @@
+package com.cw.algorithm.hot100;
+
+import java.util.*;
+
+/**
+ * @author thisdcw
+ * @date 2025年12月17日 16:25
+ */
+public class Hot15 {
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+
+        System.out.println(threeSum(nums));
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+
+        int n = nums.length;
+        for (int i = 0; i < n - 2; i++) {
+
+            if (nums[i] > 0) {
+                continue;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int l = i + 1, r = n - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    while (l<r && nums[l] == nums[l+1]) {
+                        l++;
+                    }
+                    while (l<r && nums[r] == nums[r-1]) {
+                        r--;
+                    }
+                    l++;
+                    r--;
+                } else if (sum < 0) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+
+        }
+        return res;
+    }
+}
