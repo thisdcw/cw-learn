2585a3493017bc9856b46f238030a344672d83fc
thisdcw <2839230683@qq.com>
2026-01-14 10:51:16 +0800

    update: 模块整理

--- cw-plugin/cw-plugin-api/src/main/java/com/cw/plugin/PluginContext.java
+++ cw-plugin/cw-plugin-api/src/main/java/com/cw/plugin/PluginContext.java
@@ -1,6 +1,8 @@
 package com.cw.plugin;
 
 import com.cw.plugin.spi.Plugin;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.net.URLClassLoader;
 
@@ -8,12 +10,14 @@ import java.net.URLClassLoader;
  * @author thisdcw
  * @date 2026年01月13日 11:19
  */
+@Getter
 public class PluginContext {
 
     private final String pluginId;
     private final Plugin plugin;
     private final URLClassLoader classLoader;
     private final PluginMetadata metadata;
+    @Setter
     private PluginState state;
 
     public PluginContext(String pluginId, Plugin plugin,
@@ -23,30 +27,6 @@ public class PluginContext {
         this.classLoader = classLoader;
         this.metadata = metadata;
         this.state = PluginState.INSTALLED;
-    }
-
-    public String getPluginId() {
-        return pluginId;
-    }
-
-    public Plugin getPlugin() {
-        return plugin;
-    }
-
-    public URLClassLoader getClassLoader() {
-        return classLoader;
-    }
-
-    public PluginMetadata getMetadata() {
-        return metadata;
-    }
-
-    public PluginState getState() {
-        return state;
-    }
-
-    public void setState(PluginState state) {
-        this.state = state;
     }
 
 }
---
+++ cw-scheduled/scheduled-core/src/main/java/com/cw/scheduled/core/Main.java
@@ -0,0 +1,8 @@
+package com.cw.scheduled.core;
+
+/**
+ * @author thisdcw
+ * @date 2026年01月14日 10:16
+ */
+public class Main {
+}
